f6f7b127e716ca0083765a40d8264c76e2b1a041
Achyutdhaduk <achyutdhaduk8139@gmail.com>
2024-06-11 17:41:13 +0530

    add Multer middleware for file uploads

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
+
+// cb means callback 
+
+// multer thi local file ma save thase tyathi cloudnary ma save thase 
